b0214761335962e3c4562155f9cb6f8c730abb5f
deaf.ayo <deaf.ayo@gmail.com>
2017-10-10 10:57:14 +0100

    version 5

---
+++ watch-and-code/script.js
@@ -0,0 +1,33 @@
+let todoList = {
+  todos: [],
+  displayTodos: function() {
+    if(this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos:');
+      for(let i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+      }
+    }
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
