1886d0fcb7ffbc54b7d76f0716ceaceb7e2691f2
deaf.ayo <deaf.ayo@gmail.com>
2017-10-13 12:32:08 +0100

    variable mutation and type coercion

--- jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 2/01_JavaScript_Language_Basics/script.js
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 2/01_JavaScript_Language_Basics/script.js
@@ -1,3 +1,5 @@
+// Lecture: variables
+/*
 let name = 'John';
 console.log(name);
 
@@ -11,3 +13,30 @@ console.log(`He is ${name} ${lastName} and he is ${age} years old`);
 
 let likesBugs = false;
 console.log(likesBugs);
+*/
+
+// Lecture: variables part 2
+
+let name = 'Bojack';
+let age = 52;
+
+// console.log(name + age);
+//console.log(age + age);
+
+let job, isMarried;
+
+// console.log(job);
+
+job = 'sitcom star';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old, former ' + job + '. Is he married? ' +isMarried + '.');
+
+age = 'fifty two';
+job = 'unemployed';
+
+console.log(name + ' is a ' + age + ' years old and he is ' + job + '. Is he married? ' +isMarried + '.');
+
+let watchedShow = prompt('Have you seen the Bojack Horseman show?');
+console.log(watchedShow);
+alert('Your answer: ' + watchedShow);
