81af0a338cbf8de72790e81d51f243eaf06bf200
deaf.ayo <deaf.ayo@gmail.com>
2017-10-13 12:00:04 +0100

    variables and data types

--- jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 2/01_JavaScript_Language_Basics/script.js
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 2/01_JavaScript_Language_Basics/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+let name = 'John';
+console.log(name);
+
+let lastName = 'Smith';
+console.log(lastName);
+
+let age = 26;
+console.log(age);
+
+console.log(`He is ${name} ${lastName} and he is ${age} years old`);
+
+let likesBugs = false;
+console.log(likesBugs);
