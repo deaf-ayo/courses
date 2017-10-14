2728a5b9b6882a5fe738e4ab259ddef1ff42a638
deaf.ayo <deaf.ayo@gmail.com>
2017-10-14 12:47:44 +0100

    coding challenge 2 done

--- jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 2/01_JavaScript_Language_Basics/script.js
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 2/01_JavaScript_Language_Basics/script.js
@@ -392,7 +392,7 @@ console.log(mike);
 */
 
 // Lecture: loops ---------------------------------
-
+/*
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
@@ -430,3 +430,48 @@ for (let i = 1; i <= 5; i++) {
   }
   console.log(i);
 }
+*/
+
+// CODING CHALLENGE 2 ---------------------------------
+
+/*
+1. Create an array with some years where persons were born
+2. Create an empty array (just [] )
+3. Use a loop to fill the array with the ages of the persons
+4. Use another loop to log into the console whether each person is of full age (18 or older), as well as their age
+
+5. Finally, create a function called printFullAge which receives the array of years as an argument, executes the steps 2., 3. and 4. and returns an array of true/false boolean values: true if the person is of full age (>= 18 years) and false if not (< 18 years)
+6. Call the function with two different arrays and store the results in two variables: full_1 and full_2
+
+Example input:  [1965, 2008, 1992]
+Example output: [true, false, true]
+
+Hint: you can use a loop not only to read from an array, like y[i], but also to set values in an array, like y[i] = ... You can also use the specific array methods.
+*/
+
+let yearsArray = [1989, 1952, 1966, 2004, 2013, 1999];
+let anotherYearsArray = [1824, 2002, 1924, 2016, 1953, 1042];
+
+function printFullAge(array) {
+  let agesArray = [];
+  let booleanArray = [];
+  array.forEach(function(year) {
+    agesArray.push(2017 - year);
+  });
+  for (let i = 0; i < agesArray.length; i++) {
+    let age = agesArray[i];
+    if (age >= 18) {
+      booleanArray.push(true);
+      console.log(age, 'This person is of full age.');
+    } else {
+      booleanArray.push(false);
+      console.log(age, 'This person is NOT of full age.');
+    }
+  }
+  return booleanArray;
+}
+
+let full_1 = printFullAge(yearsArray);
+let full_2 = printFullAge(anotherYearsArray);
+console.log(full_1);
+console.log(full_2);
