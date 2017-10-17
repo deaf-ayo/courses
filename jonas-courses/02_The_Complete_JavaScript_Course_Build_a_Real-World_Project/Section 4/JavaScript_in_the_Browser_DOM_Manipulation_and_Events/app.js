f766121faa145c0341da198ff6552d34836a6ebb
deaf.ayo <deaf.ayo@gmail.com>
2017-10-17 13:11:06 +0100

    Events and Event Handling: Rolling the Dice done

--- jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 4/JavaScript_in_the_Browser_DOM_Manipulation_and_Events/app.js
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 4/JavaScript_in_the_Browser_DOM_Manipulation_and_Events/app.js
@@ -9,18 +9,36 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.dice').style.display ='none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. random number
+  let dice = Math.floor(Math.random() * 6) + 1;
+  // 2. display the result
+  let diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // 3. update round score IF rolled number !1
+});
+
+
+/*
 document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 let x = document.querySelector('#score-0').textContent;
 console.log(x);
-
-document.querySelector('.dice').style.display ='none';
+*/
