3ee21d58df3e21ebbbaed62a7043647e5db22b84
deaf.ayo <deaf.ayo@gmail.com>
2017-10-17 15:07:23 +0100

    Finishing Touches: State Variables done

--- jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 4/JavaScript_in_the_Browser_DOM_Manipulation_and_Events/app.js
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 4/JavaScript_in_the_Browser_DOM_Manipulation_and_Events/app.js
@@ -9,40 +9,45 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  // 1. random number
-  let dice = Math.floor(Math.random() * 6) + 1;
-  // 2. display the result
-  let diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
-  // 3. update round score IF rolled number !1
-  if (dice !== 1) {
-    // add score
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
-  }
+  if (gamePlaying) {
+    // 1. random number
+    let dice = Math.floor(Math.random() * 6) + 1;
+    // 2. display the result
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    // 3. update round score IF rolled number !1
+    if (dice !== 1) {
+      // add score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+      } else {
+      nextPlayer();
+      }
+    }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  // add CURRENT score to GLOBAL score
-  scores[activePlayer] += roundScore;
-  // update the UI
-  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-  // check if player won the game
-  if (scores[activePlayer] >= 10) {
-    document.getElementById('name-' + activePlayer).textContent ='Winner!';
-    document.querySelector('.dice').style.display ='none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  } else {
-    nextPlayer();
+  if (gamePlaying) {
+    // add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+    // update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // check if player won the game
+    if (scores[activePlayer] >= 10) {
+      document.getElementById('name-' + activePlayer).textContent ='Winner!';
+      document.querySelector('.dice').style.display ='none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
@@ -62,6 +67,7 @@ function init() {
   scores = [0, 0];
   roundScore = 0;
   activePlayer = 0;
+  gamePlaying = true;
   document.querySelector('.dice').style.display ='none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
