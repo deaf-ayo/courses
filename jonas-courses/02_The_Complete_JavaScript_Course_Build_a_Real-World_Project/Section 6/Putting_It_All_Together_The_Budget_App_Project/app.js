85048fcaa524cb39cc7a8532fd061147d95b6ce5
deaf.ayo <deaf.ayo@gmail.com>
2017-11-13 08:10:17 +0000

    Implementing the Module Pattern

---
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/Section 6/Putting_It_All_Together_The_Budget_App_Project/app.js
@@ -0,0 +1,31 @@
+let budgetController = (function() {
+
+  let x = 23;
+  let add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b) {
+      return add(b);
+    }
+  }
+
+})();
+
+let UIController = (function() {
+
+  // code
+
+})();
+
+let controller = (function(budgetCtrl, UICtrl) {
+
+  let z = budgetCtrl.publicTest(10);
+  return {
+    anotherPublic: function() {
+      console.log(z);
+    }
+  }
+
+})(budgetController, UIController);
