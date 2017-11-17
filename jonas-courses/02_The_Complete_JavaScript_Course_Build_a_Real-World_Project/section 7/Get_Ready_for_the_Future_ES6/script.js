023e4ddf33537c12f34de65d3ea12812d53f88bc
deaf.ayo <deaf.ayo@gmail.com>
2017-11-17 16:06:45 +0000

    Destructuring

--- jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/section 7/Get_Ready_for_the_Future_ES6/script.js
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/section 7/Get_Ready_for_the_Future_ES6/script.js
@@ -181,6 +181,7 @@ const box66 = {
 box66.clickMe();
 */
 
+/*
 function Person(name) {
   this.name = name;
 }
@@ -206,3 +207,41 @@ Person.prototype.myFriends6 = function(friends) {
 var friends = ['Bob', 'Jane', 'Mark'];
 
 new Person('Mike').myFriends6(friends);
+*/
+
+// ----- lecture: destructuring  -----
+
+/*
+// ES5
+var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
+
+// ES6
+
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+  firstName: 'John',
+  lastName: 'Smith'
+}
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+*/
+
+function calcAgeRetirement(year) {
+  const age = new Date().getFullYear() - year;
+  return [age, 65 - age]
+}
+
+const [age, retirement] = calcAgeRetirement(1989);
+console.log(age);
+console.log(retirement);
