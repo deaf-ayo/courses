011d0ffc13ef1c25903eaaef234ec587039b4561
deaf.ayo <deaf.ayo@gmail.com>
2018-01-13 12:15:02 +0000

    spread operator

--- jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/section 7/Get_Ready_for_the_Future_ES6/script.js
+++ jonas-courses/02_The_Complete_JavaScript_Course_Build_a_Real-World_Project/section 7/Get_Ready_for_the_Future_ES6/script.js
@@ -292,6 +292,7 @@ for(const cur of boxesArr6) {
 
 // --
 
+/*
 // ES5
 
 var ages = [12, 17, 8, 21, 14, 11, 72];
@@ -309,3 +310,35 @@ console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
 
 console.log(ages.filter(cur => cur >= 18));
+*/
+
+// ----- lecture: spread operator  -----
+
+function addFourAges(a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5
+
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
