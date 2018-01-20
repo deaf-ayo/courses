035982a199a24fd202aac8793cc01c65dc812dfd
deaf.ayo <deaf.ayo@gmail.com>
2018-01-20 12:41:45 +0000

    Whitespace

---
+++ alicea-courses/js-understanding-the-weird-parts/Section_4/11_Whitespace/app.js
@@ -0,0 +1,22 @@
+let
+  // first name of the person
+  fname,
+
+  // last name of the person
+  lname,
+
+  // the language
+  // can be 'en' or 'es'
+  lang;
+
+let person = {
+
+  //the first name
+  fname: 'Jo',
+
+  // the last name
+  // (always required)
+  lname: 'Red'
+}
+
+console.log(person)
